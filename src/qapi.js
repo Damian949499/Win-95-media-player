637c9aef6a80e781f7a27a097fa69e8e7e5060a2
Damian949499 <134239019+Damian949499@users.noreply.github.com>
2023-05-21 19:01:24 -0400

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"646aa091dad7580021a3df38",
+				"646aa091dad7580021a3df3b"
+			]
+		},
+		"646aa091dad7580021a3df38": {
+			"id": "646aa091dad7580021a3df38",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"646aa091dad7580021a3df3b": {
+			"id": "646aa091dad7580021a3df3b",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
